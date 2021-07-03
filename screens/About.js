a0498eaf84bd32cdef9e80696a6a26edb3446615
Henry Doce <henrydoce@protonmail.com>
2021-07-02 19:25:57 -0500

    Implemented global styles

--- screens/About.js
+++ screens/About.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import globalStyles from '../styles/global'
+
 
 const About = () => {
     return (
-        <View style={styles.container}>
+        <View style={globalStyles.container}>
             <Text>About Screen</Text>
         </View>
     )
 }
 
 export default About
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 40,
-    }
-})
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import globalStyles from '../styles/global'
 
 const ReviewDetails = () => {
     return (
-        <View style={styles.container}>
+        <View style={globalStyles.container}>
             <Text>Review Details Screen</Text>
         </View>
     )
 }
 
 export default ReviewDetails
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 40,
-    }
-})
