99bef2ee0795bf25f127af62d52e43eac5a8e269
Henry Doce <henrydoce@protonmail.com>
2021-07-06 16:58:26 -0500

    Dynamic ratings now working

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-import globalStyles from '../styles/global';
+import { Text, View, Image, StyleSheet } from 'react-native';
+import globalStyles, { images } from '../styles/global';
 import Card from '../shared/card';
 
 const ReviewDetails = ({ navigation }) => {
+    const rating = navigation.getParam('rating');
+
     return (
         <View style={globalStyles.container}>
             <Card>
                 <Text>{navigation.getParam('title')}</Text>
-                <Text>{navigation.getParam('rating')}</Text>
                 <Text>{navigation.getParam('body')}</Text>
+                <View style={styles.rating}>
+                    <Text>GameZone rating: </Text>
+                    <Image source={images.ratings[rating]} />
+                </View>
             </Card>
         </View>
     )
 }
 
 export default ReviewDetails;
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row', 
+        justifyContent: 'center',
+        paddingTop: 16, 
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee'
+    }
+})
